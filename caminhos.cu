9e6325b74c32a970c525431300e5419fb85685c2
Vitor Yuske <vitor.yuski1@gmail.com>
2024-05-25 19:55:24 -0400

    Update on caminhas.cu implementation

--- caminhos.cu
+++ caminhos.cu
@@ -1,156 +1,202 @@
 // ----------------------------------------------------------------------------
+// Nome dos alunos do grupo:
+// Vitor Yuske Watanabe - 2020.1905.058-4
+// Raissa Rinaldi Yoshioka - 2020.1905.049-5
+// ----------------------------------------------------------------------------
 // Dado um grafo direcionado com pesos nas arestas, encontrar distância do caminho mais curto entre cada par de vértices.
 // Supor:
 //		Arestas possuem pesos > 0
 //		Grafo não possui loops (aresta de um vértice para ele mesmo)
 //		Número de vértices do grafo é potência de 2
 //
-// Para compilar: gcc caminhos_seq.c -o caminhos_seq
-// Para executar: ./caminhos_seq nVértices arquivoEntrada arquivoSaída
+// Para compilar: nvcc caminhos.cu -o caminhos
+// Para executar: ./caminhos nVértices arquivoEntrada arquivoSaída
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-#include <sys/time.h>
+
+// ----------------------------------------------------------------------------
+// Macro para checagem de erro das chamadas às funções do CUDA
+#define checa_cuda(result)                          \
+    if (result != cudaSuccess)                      \
+    {                                               \
+        printf("%s\n", cudaGetErrorString(result)); \
+        exit(1);                                    \
+    }
 
 // ----------------------------------------------------------------------------
 void leArqIn(char *nomeArqIn, int nElem, unsigned int *mat)
 {
-	unsigned int p;
+    unsigned int p;
 
-	FILE *arqIn = fopen(nomeArqIn, "rt");	// Arquivo texto de entrada
+    FILE *arqIn = fopen(nomeArqIn, "rt"); // Arquivo texto de entrada
 
-	if (arqIn == NULL)
-	{
-		printf("\nArquivo texto de entrada não encontrado\n");
-		exit(1);
-	}
+    if (arqIn == NULL)
+    {
+        printf("\nArquivo texto de entrada não encontrado\n");
+        exit(1);
+    }
 
-	// Lê matriz nElem x nElem de adjacências do arquivo de entrada
-	// Inicializa matriz nElem x nElem de distâncias com distâncias mínimas de caminhos com até 1 aresta
-	for (int i = 0; i < nElem; i++)
-		for (int j = 0; j < nElem; j++)
-		{
-			fscanf(arqIn, "%u", &p); // peso da aresta (i,j)
-			// Peso 0 indica ausência de aresta de i para j
-			if (p == 0 && i != j)
-				mat[i*nElem + j] = INT_MAX; // mat[i][j] = infinito
-			else
-				mat[i*nElem + j] = p; // mat[i][j] = adj[i][j] = peso da aresta (i,j)
-		}
+    // Lê matriz nElem x nElem de adjacências do arquivo de entrada
+    // Inicializa matriz nElem x nElem de distâncias com distâncias mínimas de caminhos com até 1 aresta
+    for (int i = 0; i < nElem; i++)
+        for (int j = 0; j < nElem; j++)
+        {
+            fscanf(arqIn, "%u", &p); // peso da aresta (i,j)
+            // Peso 0 indica ausência de aresta de i para j
+            if (p == 0 && i != j)
+                mat[i * nElem + j] = INT_MAX; // mat[i][j] = infinito
+            else
+                mat[i * nElem + j] = p; // mat[i][j] = adj[i][j] = peso da aresta (i,j)
+        }
 
-	fclose(arqIn);
+    fclose(arqIn);
 }
 
 // ----------------------------------------------------------------------------
-void calculaDist(int nElem, unsigned int *matIn, unsigned int *matOut)
+__global__ void calculaDist(int nElem, unsigned int *matIn, unsigned int *matOut)
 {
-	unsigned int dij, dikj;
+	// linha i = coordenada y do id GLOBAL da thread
+	int i = blockIdx.y * blockDim.y + threadIdx.y;
+	// coluna j = coordenada x do id GLOBAL da thread
+	int j = blockIdx.x * blockDim.x + threadIdx.x;
 
-	// Para cada par de vértices i e j
-	for (int i = 0; i < nElem; i++)
-		for (int j = 0; j < nElem; j++)
-		{
-			// Calcula distância mínima de i para j, testando,
-			// para cada vértice k, distância de i para k + distância de k para j
-			dij = INT_MAX;
-			for (int k = 0; k < nElem; k++)
-			{
-				dikj = matIn[i*nElem + k] + matIn[k*nElem + j]; // matIn[i][k] + matIn[k][j]
-				if (dikj < dij)
-					dij = dikj;
-			}
-			matOut[i*nElem + j] = dij; // matOut[i][j]
-		}
+	// Se thread corresponde a uma célula da matriz C
+	if ((i < nElem) && (j < nElem))
+	{
+        unsigned int dij = INT_MAX, dikj;
+        for (int k = 0; k < nElem; k++)
+        {
+            dikj = matIn[i * nElem + k] + matIn[k * nElem + j]; // matIn[i][k] + matIn[k][j]
+            if (dikj < dij)
+                dij = dikj;
+        }
+        matOut[i * nElem + j] = dij; // matOut[i][j]
+	}
 }
 
 // ----------------------------------------------------------------------------
-void escreveArqOut(char* nomeArqOut, int nElem, unsigned int *mat)
+void escreveArqOut(char *nomeArqOut, int nElem, unsigned int *mat)
 {
-	FILE *arqOut;	// Arquivo texto de saída
+    FILE *arqOut; // Arquivo texto de saída
 
-	arqOut = fopen(nomeArqOut, "wt");
+    arqOut = fopen(nomeArqOut, "wt");
 
-	// Escreve matriz nElem x nElem de distâncias no arquivo de saída
-	for (int i = 0; i < nElem; i++)
-	{
-		for (int j = 0; j < nElem; j++)
-			if (mat[i*nElem + j] != INT_MAX)	// mat[i][j]
-				fprintf(arqOut, "%d ", mat[i*nElem + j]); // Distância mínima de i para j
-			else
-				fprintf(arqOut, "-1 ");	// Não há caminho de i para j
-		fprintf(arqOut, "\n");
-	}
+    // Escreve matriz nElem x nElem de distâncias no arquivo de saída
+    for (int i = 0; i < nElem; i++)
+    {
+        for (int j = 0; j < nElem; j++)
+            if (mat[i * nElem + j] != INT_MAX)              // mat[i][j]
+                fprintf(arqOut, "%d ", mat[i * nElem + j]); // Distância mínima de i para j
+            else
+                fprintf(arqOut, "-1 "); // Não há caminho de i para j
+        fprintf(arqOut, "\n");
+    }
 
-	fclose(arqOut);
+    fclose(arqOut);
 }
 
 // ----------------------------------------------------------------------------
 // Programa principal
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	if(argc != 4)
-	{
-		printf("O programa foi executado com argumentos incorretos.\n");
-		printf("Uso: ./caminhos_seq nVértices arquivoEntrada arquivoSaída\n");
-		exit(1);
-	}
+    if (argc != 4)
+    {
+        printf("O programa foi executado com argumentos incorretos.\n");
+        printf("Uso: ./caminhos_seq nVértices arquivoEntrada arquivoSaída\n");
+        exit(1);
+    }
 
-	int nVert = atoi(argv[1]);	// Obtém número de vértices do grafo
+    int nVert = atoi(argv[1]); // Obtém número de vértices do grafo
 
-	// Obtém nome dos arquivos de entrada e saída
-	char nomeArqIn[100],
-		  nomeArqOut[100] ;
+    // Obtém nome dos arquivos de entrada e saída
+    char nomeArqIn[100],
+        nomeArqOut[100];
 
-	strcpy(nomeArqIn, argv[2]) ;
-	strcpy(nomeArqOut, argv[3]) ;
+    strcpy(nomeArqIn, argv[2]);
+    strcpy(nomeArqOut, argv[3]);
 
-	// Aloca matrizes nVert x nVert de distâncias mais curtas
-	int nBytes = nVert * nVert * sizeof(int);
-	unsigned int *distIn = (unsigned int *) malloc(nBytes);
-	unsigned int *distOut = (unsigned int *) malloc(nBytes);
-	unsigned int *auxTroca;
-	if (distIn == NULL || distOut == NULL)
-	{
-		printf("\nErro na alocação das estruturas de dados\n");
-		exit(1);
-	}
+    // Cria variáveis
+    unsigned int nBytes = nVert * nVert * sizeof(int), // Tamanho dos vetores em bytes
+        *distInHost, *distInDevice, *distOutDevice;
 
-	// Lê matriz de adjacências do arquivo de entrada e inicializa matriz de distâncias
-	leArqIn(nomeArqIn, nVert, distIn);
+    // Aloca o vetor no host
+    distInHost = (unsigned int *)malloc(nBytes);
+    if (distInHost == NULL)
+    {
+        printf("\nErro na alocação das estruturas de dados\n");
+        exit(1);
+    }
 
-	// Inicia medição do tempo de execução
-	struct timeval h_ini, h_fim;
-	gettimeofday(&h_ini, 0);
+    // Lê matriz de adjacências do arquivo de entrada e inicializa matriz de distâncias
+    leArqIn(nomeArqIn, nVert, distInHost);
 
-	// Repete log_2(nVert) passos
-	for (int alcance = 2; alcance <= nVert; alcance<<=1)
-	{
-		// Usando distâncias mínimas de caminhos com até (alcance) arestas,
-		// calcula distâncias mínimas de caminhos com até (2*alcance) arestas
-		calculaDist(nVert, distIn, distOut);
+    // Aloca vetores na memória global da GPU
+    checa_cuda(cudaMalloc((void **)&distInDevice, nBytes));
+    checa_cuda(cudaMalloc((void **)&distOutDevice, nBytes));
 
-		// Troca ponteiros de matrizes de distâncias distIn e distOut
-		auxTroca = distIn;
-		distIn   = distOut;
-		distOut  = auxTroca;
-	}
-	
-	// Finaliza medição do tempo de execução
-	gettimeofday(&h_fim, 0);
-	long segundos = h_fim.tv_sec - h_ini.tv_sec;
-	long microsegundos = h_fim.tv_usec - h_ini.tv_usec;
-	double h_tempo = (segundos * 1e3) + (microsegundos * 1e-3); // Tempo de execução na CPU em ms
+    // Inicia medição de tempo de execução na GPU
+    cudaEvent_t d_ini, d_fim;
+    cudaEventCreate(&d_ini);
+    cudaEventCreate(&d_fim);
+    cudaEventRecord(d_ini, 0);
+        
+	// Determina nBlocos e nThreadsBloco
+	// nBlocos.x = teto(m / nThreadsBloco.x)
+	// nBlocos.y = teto(n / nThreadsBloco.y)
+	dim3 nThreadsBloco(32,32);
+	dim3 nBlocos((nVert + (nThreadsBloco.x - 1)) / nThreadsBloco.x, (nVert + (nThreadsBloco.y - 1)) / nThreadsBloco.y);
 
-	// Escreve matriz de distâncias no arquivo de saída
-	escreveArqOut(nomeArqOut, nVert, distIn);
+    // Copia dados de entrada do host para memória global da GPU
+    checa_cuda(cudaMemcpy(distInDevice, distInHost, nBytes, cudaMemcpyHostToDevice));
 
-	printf("Tempo CPU = %.2fms\n", h_tempo);
+    // Repete log_2(nVert) passos
+    for (int alcance = 2; alcance <= nVert; alcance <<= 1)
+    {
+        // Usando distâncias mínimas de caminhos com até (alcance) arestas,
+        // calcula distâncias mínimas de caminhos com até (2*alcance) arestas
+        calculaDist<<<nBlocos, nThreadsBloco>>>(nVert, distInDevice, distOutDevice);
 
-	// Libera matrizes
-	free(distIn);
-	free(distOut);
+        // Host aguarda a execução da GPU
+        cudaDeviceSynchronize();
 
-	return 0;
+        // Atribui os valores de distOutDevice em distInDevice
+        checa_cuda(cudaMemcpy(distInHost, distOutDevice, nBytes, cudaMemcpyDeviceToHost));
+
+        // for (int i = 0; i < nVert; i++){
+        //     for (int j = 0; j < nVert; j++){
+        //         printf("%d ", distInHost[i * nVert + j]);
+        //     }
+        //     printf("\n");
+        // }
+
+        checa_cuda(cudaMemcpy(distInDevice, distInHost, nBytes, cudaMemcpyHostToDevice));
+        // checa_cuda(cudaMemcpy(distInDevice, distOutDevice, nBytes, cudaMemcpyDeviceToDevice));
+    }
+
+    // Copia dados de entrada da GPU para o host
+    checa_cuda(cudaMemcpy(distInHost, distInDevice, nBytes, cudaMemcpyDeviceToHost));
+
+    // Finaliza medição do tempo de execução
+    cudaEventRecord(d_fim, 0);
+    cudaEventSynchronize(d_fim);
+    float d_tempo; // Tempo de execução na GPU em milissegundos
+    cudaEventElapsedTime(&d_tempo, d_ini, d_fim);
+    cudaEventDestroy(d_ini);
+    cudaEventDestroy(d_fim);
+    printf("Tempo GPU = %.2fms\n", d_tempo);
+
+    // Escreve matriz de distâncias no arquivo de saída
+    escreveArqOut(nomeArqOut, nVert, distInHost);
+
+    // Libera vetores na memória global da GPU
+    checa_cuda(cudaFree(distInDevice));
+    checa_cuda(cudaFree(distOutDevice));
+
+    // Libera vetor no host
+    free(distInHost);
+
+    return 0;
 }
--- teste.sh
+++ teste.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-gcc caminhos_seq.c -o caminhos_seq -Wall
-# gcc esparso_par.c -fopenmp -o esparso_par.o -Wall
+gcc caminhos_seq.c -o caminhos_seq
+nvcc caminhos.cu -o caminhos
 
 declare -a n_vertices=( 4 8 32 256 512 )
+# declare -a n_vertices=( 256 )
 
 for i in "${!n_vertices[@]}"
 do
-    ./caminhos_seq ${n_vertices[$i]} ./Entradas_Saidas/e$(($i + 1)).txt ./Entradas_Saidas/saida_teste$(($i + 1)).txt
-    # ./esparso_par.o ./Entradas/e$i.txt ./Saidas/par_s$i.txt
-done
-
-for i in {1..5}
-do
-    cmp --silent ./Entradas_Saidas/saida$i.txt ./Entradas_Saidas/saida_teste$i.txt && echo "Test case $i: Files are EQUAL" || echo "Test case $i: Files are DIFFERENT"
+    ./caminhos_seq ${n_vertices[$i]} ./Entradas_Saidas/e$(($i + 1)).txt ./Entradas_Saidas/saida$(($i + 1)).txt
+    ./caminhos ${n_vertices[$i]} ./Entradas_Saidas/e$(($i + 1)).txt ./Entradas_Saidas/saida_par$(($i + 1)).txt
+    cmp --silent ./Entradas_Saidas/saida$(($i + 1)).txt ./Entradas_Saidas/saida_par$(($i + 1)).txt && echo "Test case $(($i + 1)): Files are EQUAL" || echo "Test case $(($i + 1)): Files are DIFFERENT"
 done
